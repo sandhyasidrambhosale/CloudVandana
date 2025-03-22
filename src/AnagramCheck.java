a5b77a40274683b16eebfbdac8caa056b5c6da70
sandhya bhosale <sandhyabhosale29@gmail.com>
2025-03-22 17:12:47 +0530

    aded java anagram check

---
+++ src/AnagramCheck.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+
+public class AnagramCheck {
+    public static void main(String[] args) {
+        String str1 = "listen";
+        String str2 = "silent";
+
+        if (areAnagrams(str1, str2)) {
+            System.out.println("Output: true");
+        } else {
+            System.out.println("Output: false");
+        }
+    }
+
+    public static boolean areAnagrams(String str1, String str2) {
+        str1 = str1.replaceAll("\\s", "").toLowerCase();
+        str2 = str2.replaceAll("\\s", "").toLowerCase();
+
+        if (str1.length() != str2.length()) {
+            return false;
+        }
+
+        char[] charArray1 = str1.toCharArray();
+        char[] charArray2 = str2.toCharArray();
+
+        Arrays.sort(charArray1);
+        Arrays.sort(charArray2);
+
+        return Arrays.equals(charArray1, charArray2);
+    }
+}
