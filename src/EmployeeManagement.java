4c29f3e5c697cbc883197017d2106059858cef66
sandhya bhosale <sandhyabhosale29@gmail.com>
2025-03-22 17:16:11 +0530

    added em system

---
+++ src/EmployeeManagement.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Employee {
+    int id;
+    String name;
+    double salary;
+
+    public Employee(int id, String name, double salary) {
+        this.id = id;
+        this.name = name;
+        this.salary = salary;
+    }
+
+    public void displayDetails() {
+        System.out.println("ID: " + id + ", Name: " + name + ", Salary: $" + salary);
+    }
+}
+
+public class EmployeeManagement {
+    public static void main(String[] args) {
+        Employee emp1 = new Employee(1, "sandhya bhosale", 50000);
+        Employee emp2 = new Employee(2, "surekha sonawane", 60000);
+        Employee emp3 = new Employee(3, "Ashwini meshram", 70000);
+
+        List<Employee> employees = new ArrayList<>();
+        employees.add(emp1);
+        employees.add(emp2);
+        employees.add(emp3);
+
+        for (Employee emp : employees) {
+            emp.displayDetails();
+        }
+    }
+}
