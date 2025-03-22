e83319294e4921bb5c057fd5d9faf64f8586d9f5
sandhya bhosale <sandhyabhosale29@gmail.com>
2025-03-22 23:21:20 +0530

    last change

--- question3/Employee.java
+++ question3/Employee.java
@@ -17,9 +17,9 @@ class Employee {
 
     public static void main(String[] args) {
         List<Employee> employees = Arrays.asList(
-            new Employee(1, "John Doe", 50000),
-            new Employee(2, "Jane Smith", 60000),
-            new Employee(3, "Robert Brown", 70000)
+            new Employee(1, "Sandhya Bhosale", 50000),
+            new Employee(2, "Surekha Sonawane", 60000),
+            new Employee(3, "Ashwini Meshram", 70000)
         );
 
         employees.forEach(Employee::displayDetails);
